8d948c1ba61bd1f2f4b22d7befa003f1f537809d
Roei Keisar <roei.keisar@gmail.com>
2023-03-09 16:02:12 +0200

    fixed script

--- script.sh
+++ script.sh
@@ -16,4 +16,5 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER && newgrp docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose up -d
+echo starting containers
+docker-compose up -d --build
